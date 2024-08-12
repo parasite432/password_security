06df79554c958f2e5d025d58a5e23d60c4010168
Jibin Gallistus Gnanadhas <jibingallistus@gmail.com>
2024-08-12 23:42:34 +1000

    Add strength and crack time

--- src/App.js
+++ src/App.js
@@ -1,16 +1,61 @@
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
+import { colors } from "./colors.js";
 import "./App.css";
+import { getValue } from "@testing-library/user-event/dist/utils/index.js";
+
+const zxcvbn = require("zxcvbn");
 
 function App() {
+  const [password, setPassword] = useState("");
+  const [strength, setStrength] = useState("very weak");
+  const [time, setTime] = useState(" less than a second");
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    if (password) {
+      const result = zxcvbn(password);
+      setScore(result.score);
+      setStrength(colors[result.score].strength);
+      setTime(result.crack_times_display.online_no_throttling_10_per_second);
+    } else {
+      setScore(0);
+      setStrength("very weak");
+      setTime("less than a second");
+    }
+  }, [password]);
+
   return (
     <div className="App">
-      <div className="App-header">
-        <p>How strong is your password?</p>
+      <div className="App-container">
+        <p className="Header">How strong is your password?</p>
         <input
           placeholder="Evaluate your password..."
           name="passwordInput"
           className="passwordBox"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
         />
+        <p className="strength">
+          your password is{" "}
+          <p
+            style={{
+              display: "inline",
+              color: colors[score].color,
+            }}
+          >
+            {strength}
+          </p>{" "}
+          and can be cracked in{" "}
+          <p
+            style={{
+              display: "inline",
+              color: colors[score].color,
+            }}
+          >
+            {time}
+          </p>
+        </p>
       </div>
     </div>
   );
---
+++ src/colors.js
@@ -0,0 +1,27 @@
+export const colors = [
+  {
+    id: 0, // Used in JSX as a key
+    color: "#FF0000",
+    strength: "very weak",
+  },
+  {
+    id: 1, // Used in JSX as a key
+    color: "#FFBA25",
+    strength: "weak",
+  },
+  {
+    id: 2, // Used in JSX as a key
+    color: "#299AA3",
+    strength: "good",
+  },
+  {
+    id: 3, // Used in JSX as a key
+    color: "#0045AC",
+    strength: "strong",
+  },
+  {
+    id: 4, // Used in JSX as a key
+    color: "#0E7A00",
+    strength: "very strong",
+  },
+];
